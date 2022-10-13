ee3b7d2ed27c46831a03b24dfac67220743527be
RyanDeguara <67072365+RyanDeguara@users.noreply.github.com>
2022-10-13 20:05:41 +0100

    lab4-changes

--- getting-started-master/app/src/static/js/app.js
+++ getting-started-master/app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above! Updated JS to have new text example....</p>
+                <p className="text-center">This was edited by C20309873</p>
             )}
             {items.map(item => (
                 <ItemDisplay
